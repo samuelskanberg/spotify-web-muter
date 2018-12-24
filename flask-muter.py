192455df8eeffd2dee32a1c8959155a98ef32a8c
Samuel Skanberg <samuel.skanberg@gmail.com>
2018-12-24 21:22:35 +0100

    Print out the javascript to be pasted

--- flask-muter.py
+++ flask-muter.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 from flask import Flask, request
+from flask_cors import CORS
 import argparse
 import sys
 import subprocess
 app = Flask(__name__)
+CORS(app)
 
 contains = ""
 
+javascript_to_paste_template = """
+function postChangedTitle(title) {
+    var xhr = new XMLHttpRequest();
+    var url = "URL_PLACE_HOLDER";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = xhr.responseText;
+            console.log("Response: "+response);
+        }
+    };
+    var data = JSON.stringify({"title": title });
+    xhr.send(data);
+}
+new MutationObserver(function() {
+    console.log("New title:" +document.title);
+    postChangedTitle(document.title);
+}).observe(document.querySelector('title'),{ childList: true });
+"""
 
 def mute():
     print("Muting")
@@ -45,4 +67,8 @@ if __name__ == '__main__':
     port = args.port
     print("Contains is: {}".format(contains))
     print("Port is: {}".format(port))
+
+    javascript_to_paste = javascript_to_paste_template.replace("URL_PLACE_HOLDER", "http://127.0.0.1:{}/".format(port))
+    print("Paste the following in the console: \n {} \n\n".format(javascript_to_paste))
+
     app.run(host='0.0.0.0', port=port, debug=True)
