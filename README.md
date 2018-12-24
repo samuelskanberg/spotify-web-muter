8d0e635166bf51c600ad7d58b22952fbe845f005
Samuel Skanberg <samuel.skanberg@gmail.com>
2018-12-24 20:46:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Flask muter
+
+Install
+
+```
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+Run
+
+```
+FLASK_APP=flask-muter.py flask run
+```
---
+++ flask-muter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+import argparse
+import sys
+app = Flask(__name__)
+
+contains = ""
+
+
+def mute():
+    print("Muting")
+
+def unmute():
+    print("Unmuting")
+
+@app.route("/", methods=['POST'])
+def hello():
+    data = request.json
+    if "title" not in data:
+        return "No title supplied"
+    title = data['title']
+    print("Title is: {}".format(title))
+    print("Title should contain: {}".format(contains))
+    if contains in title:
+        unmute()
+        return "Unmute"
+    else:
+        mute()
+        return "Mute"
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("contains", type=str,
+                    help="A text which the title should contain")
+    parser.add_argument("-p", "--port", type=int, default=5000,
+                    help="Port number the webserver will run under")
+    args = parser.parse_args()
+
+    contains = args.contains
+    port = args.port
+    print("Contains is: {}".format(contains))
+    print("Port is: {}".format(port))
+    app.run(host='0.0.0.0', port=port, debug=True)
